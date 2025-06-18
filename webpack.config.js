9f905a282fdafcd717ca0ec4508d322fbc895aef
code2344 <71059013+code2344@users.noreply.github.com>
2025-06-18 13:42:39 +1000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
     historyApiFallback: true,
     noInfo: true,
     overlay: true,
-    allowedHosts: "all"
+    allowedHosts: ['all']
   },
   performance: {
     hints: false
